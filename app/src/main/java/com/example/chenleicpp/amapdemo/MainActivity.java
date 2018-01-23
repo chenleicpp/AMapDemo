e3d21e56ddd4818a44ba5aa56fc0e95be78213d4
chenlei <chenleicpp@163.com>
2018-01-23 23:19:44 +0800

    定位图标的修改

--- app/src/main/java/com/example/chenleicpp/amapdemo/MainActivity.java
+++ app/src/main/java/com/example/chenleicpp/amapdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chenleicpp.amapdemo;
 
 import android.Manifest;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,8 @@ import android.util.Log;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.tbruyelle.rxpermissions2.Permission;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
@@ -37,10 +40,29 @@ public class MainActivity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mapView.getMap();
             aMap.getUiSettings().setRotateGesturesEnabled(false);
-            aMap.getUiSettings().setMyLocationButtonEnabled(true);
             aMap.moveCamera(CameraUpdateFactory.zoomBy(6));
-            aMap.setMyLocationEnabled(true);
+            setUpMap();
         }
+    }
+
+    private void setUpMap() {
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        // 自定义系统定位蓝点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        // 自定义定位蓝点图标
+        myLocationStyle.myLocationIcon(
+                BitmapDescriptorFactory.fromResource(R.drawable.gps_point));
+        // 自定义精度范围的圆形边框颜色
+        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));
+        // 自定义精度范围的圆形边框宽度
+        myLocationStyle.strokeWidth(0);
+        // 设置圆形的填充颜色
+        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));
+        // 将自定义的 myLocationStyle 对象添加到地图上
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
     }
 
     private void requestPermissions(){
